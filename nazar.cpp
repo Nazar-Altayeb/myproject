97049d0937e9e88995611aceb43d1627d57ec4e1
Nazar-Altayeb <70408289+Nazar-Altayeb@users.noreply.github.com>
2020-11-10 22:04:37 +0200

    this is the first version

---
+++ nazar.cpp
@@ -0,0 +1,54 @@
+/*My name: Nazar Altayeb Osman Alkhalifa
+ Class: second
+ Department: Computer */
+#include <iostream>//importing iostream for input and output system
+#include<string>
+
+
+using namespace std;
+
+int main()//the main function
+{
+    string name, department;//defining string variables
+    char depart, dgree;//defining char variables
+    int  student_id, mark;
+    cout << "Enter student name: "; getline(cin, name);//taking an input and store it in name variable
+    cout << "Enter student Id: "; cin >> student_id;
+    cout << "(A) control \n(T) Telecommunication: \n(c)Computer: \nChose Department: "; cin >> depart;//taking an input as char and store it in depart variable
+    cout << "Enter student dgree: "; cin >> mark;//taking an input as mark and store it in mark variable
+    if (depart == 'A') //if condition to check if that is student Department
+        department = "Control";
+
+    else if(depart =='T'){
+        department = "Telecomunication";}
+    else if(depart =='C')
+        department ="Computer";
+    else
+        department="Department out of list";
+    switch ( mark) { //here switch used for taking deside and specify the dgree of student
+    case 50 ... 59:
+        dgree = 'F';//if the case above is tree store "F" in dgree variable
+        break; //break in switch use to prevent entering in second case
+    case 60 ... 69://this case in range from 60 to 69
+        dgree = 'D';//if the case above is tree store "D" in dgree variable
+        break;
+    case 70 ... 79://this case one in range from to
+        dgree = 'C';//if the case above is tree store "C" in dgree variable
+        break;
+    case 80 ... 89://this case in range from 80 to 89
+        dgree = 'B';//if the case above is tree store "B" in dgree variable
+        break;
+    case 90 ... 100://this case in range from 90 to 100
+        dgree = 'A';//if the case above is tree store "A" in dgree variable
+        break;
+    default:
+        string dgree = "invalid Selection";
+
+
+    };
+    cout << "Student name: " << name << endl;//this line to print out student name
+    cout << "Student ID: " << student_id << endl;//this line to print out student ID
+    cout << "Student degree: " << dgree << endl;//this line to print out student dgree
+    cout << "Department: " << department << endl;//this line to print out student Department
+    return 0;
+}
